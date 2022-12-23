9ec2e3ae7313680c719ec784adab2542df582a25
Robin Genz <mail@robingenz.dev>
2022-12-23 20:59:25 +0100

    fix(android): catch all native errors

--- android/src/main/java/io/capawesome/capacitorjs/plugins/cloudinary/CloudinaryPlugin.java
+++ android/src/main/java/io/capawesome/capacitorjs/plugins/cloudinary/CloudinaryPlugin.java
@@ -32,91 +32,103 @@ public class CloudinaryPlugin extends Plugin {
 
     @PluginMethod
     public void initialize(PluginCall call) {
-        if (initialized) {
-            return;
-        }
-        String cloudName = call.getString("cloudName");
-        if (cloudName == null) {
-            call.reject(ERROR_CLOUD_NAME_MISSING);
-            return;
-        }
+        try {
+            if (initialized) {
+                return;
+            }
+            String cloudName = call.getString("cloudName");
+            if (cloudName == null) {
+                call.reject(ERROR_CLOUD_NAME_MISSING);
+                return;
+            }
 
-        implementation.initialize(cloudName);
-        initialized = true;
-        call.resolve();
+            implementation.initialize(cloudName);
+            initialized = true;
+            call.resolve();
+        } catch (Exception ex) {
+            call.reject(ex.getMessage());
+        }
     }
 
     @PluginMethod
     public void uploadResource(PluginCall call) {
-        if (!initialized) {
-            call.reject(ERROR_NOT_INITIALIZED);
-            return;
-        }
-        String resourceType = call.getString("resourceType");
-        if (resourceType == null) {
-            call.reject(ERROR_RESOURCE_TYPE_MISSING);
-            return;
-        }
-        String uploadPreset = call.getString("uploadPreset");
-        if (uploadPreset == null) {
-            call.reject(ERROR_UPLOAD_PRESET_MISSING);
-            return;
-        }
-        String path = call.getString("path");
-        if (path == null) {
-            call.reject(ERROR_PATH_MISSING);
-            return;
-        }
-        String publicId = call.getString("publicId");
-
-        implementation.uploadResource(
-            resourceType,
-            path,
-            uploadPreset,
-            publicId,
-            new UploadResourceResultCallback() {
-                @Override
-                public void success(Map resultData) {
-                    JSObject result = CloudinaryHelper.createUploadResourceResult(resultData);
-                    call.resolve(result);
-                }
-
-                @Override
-                public void error(String message) {
-                    call.reject(message);
-                }
+        try {
+            if (!initialized) {
+                call.reject(ERROR_NOT_INITIALIZED);
+                return;
             }
-        );
+            String resourceType = call.getString("resourceType");
+            if (resourceType == null) {
+                call.reject(ERROR_RESOURCE_TYPE_MISSING);
+                return;
+            }
+            String uploadPreset = call.getString("uploadPreset");
+            if (uploadPreset == null) {
+                call.reject(ERROR_UPLOAD_PRESET_MISSING);
+                return;
+            }
+            String path = call.getString("path");
+            if (path == null) {
+                call.reject(ERROR_PATH_MISSING);
+                return;
+            }
+            String publicId = call.getString("publicId");
+
+            implementation.uploadResource(
+                resourceType,
+                path,
+                uploadPreset,
+                publicId,
+                new UploadResourceResultCallback() {
+                    @Override
+                    public void success(Map resultData) {
+                        JSObject result = CloudinaryHelper.createUploadResourceResult(resultData);
+                        call.resolve(result);
+                    }
+
+                    @Override
+                    public void error(String message) {
+                        call.reject(message);
+                    }
+                }
+            );
+        } catch (Exception ex) {
+            call.reject(ex.getMessage());
+        }
     }
 
     @PluginMethod
     public void downloadResource(PluginCall call) {
-        if (!initialized) {
-            call.reject(ERROR_NOT_INITIALIZED);
-            return;
-        }
-        String url = call.getString("url");
-        if (url == null) {
-            call.reject(ERROR_URL_MISSING);
-            return;
-        }
-
-        implementation.downloadResource(
-            url,
-            new DownloadResourceResultCallback() {
-                @Override
-                public void success(String path) {
-                    JSObject result = new JSObject();
-                    result.put("path", path);
-                    call.resolve(result);
-                }
-
-                @Override
-                public void error(String message) {
-                    call.reject(message);
-                }
+        try {
+            if (!initialized) {
+                call.reject(ERROR_NOT_INITIALIZED);
+                return;
             }
-        );
+            String url = call.getString("url");
+            if (url == null) {
+                call.reject(ERROR_URL_MISSING);
+                return;
+            }
+
+            implementation.downloadResource(
+                url,
+                new DownloadResourceResultCallback() {
+                    @Override
+                    public void success(String path) {
+                        JSObject result = new JSObject();
+                        result.put("path", path);
+                        call.resolve(result);
+                    }
+
+                    @Override
+                    public void error(String message) {
+                        call.reject(message);
+                    }
+                }
+            );
+        } catch (Exception ex) {
+            call.reject(ex.getMessage());
+        }
     }
 
     public Cloudinary getImplementation() {
